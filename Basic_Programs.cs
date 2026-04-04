e6de9af506fcd2f477bdfc7aa98df5a0b4482a8c
Shashikant-Tayade <shashibtayade@gmail.com>
2026-04-04 20:03:36 +0530

    Add project files.

---
+++ Basic_Programs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{
+    internal class Basic_Programs
+    {
+        // calling methods from this main method.
+        //Reverse String using blank string.
+
+        //Logic: take a string from user. populate string in for loop in 
+        // reverse direction from last to first. store it in blank string.
+        public string ReverseStringUsingBlankString(string ipstring)
+        {
+            string RevString = "";
+
+            for (int i = ipstring.Length - 1; i >= 0; i--)
+            {
+                RevString = RevString + ipstring[i];
+            }
+            return RevString;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic_Programs Basic_Programs_Obj = new Basic_Programs();
+
+            Console.WriteLine("Enter the string to reverse : ");
+            string ipstring = Console.ReadLine();
+
+            string RevString = Basic_Programs_Obj.ReverseStringUsingBlankString(ipstring);
+            Console.WriteLine($"Reversed String = {RevString}");
+
+            Console.ReadLine();
+        }
+    }
+}
