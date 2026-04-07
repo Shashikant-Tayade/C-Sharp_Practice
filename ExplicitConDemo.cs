767a45ab882bb0ca461f1fedb16db9d6988c532f
Shashikant-Tayade <shashibtayade@gmail.com>
2026-04-07 22:45:59 +0530

    Explicit Constructor Demo

---
+++ ExplicitConDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{
+    internal class ExplicitConDemo
+    {
+        // Explicit constructor is defined by programmer. 
+        // and called when created class object.
+        public ExplicitConDemo()
+        {
+            Console.WriteLine("Explicit Constructor called");
+        }
+        static void Main(string[] args)
+        {
+            ExplicitConDemo explicitConDemoObj = new ExplicitConDemo();
+
+        }
+    }
+}
